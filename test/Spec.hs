b1ff1b45779263691fd56db40078eaf587361cd9
Graham <graham.gibbons@gmail.com>
2017-12-18 18:10:45 -0800

    Add basic tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,20 @@
+module Main where
+
+import           Test.Tasty
+import           Test.Tasty.HUnit
+
+import           GeometricAlgebra
+
+a = fromVector $ Vector 'a'
+b = fromVector $ Vector 'b'
+c = fromVector $ Vector 'c'
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  defaultMain
+    $ testGroup "Tests" [
+      testCase "Anticommutivity" $
+        a * b @?= -(b * a)
+    , testCase "Associativity" $
+        a * (b * c )@?= (a * b) * c
+    ]
